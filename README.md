9c4ed5fd14c5a48fcb261eb530ba3975be9b4378
kbakun <krzysztof@bakun.biz>
2014-11-13 11:48:11 +0100

    website update

--- README.md
+++
@@ -1,9 +0,0 @@
-milo-test2
-==========
-
-Aplication - src
-
-tests - test
-
-Compiled version - dist
-
--- src/components/navbar/navbar.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('test2')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.date = new Date();
-  });
